8ee712c015cd22bfa6376ca692d0d81e51988fba
gokul2736 <gokulm2732006@gmail.com>
2026-04-24 03:39:09 +0530

    code file

---
+++ test2.py
@@ -0,0 +1,8 @@
+a = 0 == True
+b = False == False
+c = True + True
+d = False + 9
+print("a is", a)
+print("b is", b)
+print("c:", c)
+print("d:", d)
---
+++ test4.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+x = complex(a, b)
+print(x)
+print(x.real)
+print(x.imag)
